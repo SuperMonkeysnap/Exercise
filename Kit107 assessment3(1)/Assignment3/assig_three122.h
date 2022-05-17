50c95814798874054590e73011145f2adf67c980
Harislwt <103138122+Yaphetskrrrrr@users.noreply.github.com>
2022-05-17 21:00:13 +1000

    Add files via upload

---
+++ Kit107 assessment3(1)/Assignment3/assig_three122.h
@@ -0,0 +1,25 @@
+/*
+ *	Sundry Definitions
+ *	Author: J.R.Dermoudy
+ *	Version: May 2022
+ *	
+ *	This file holds sundry definitions relating to the Assignment.
+ *
+ *	You will need to change these during development.
+ */
+
+#include <stdbool.h>
+
+#ifndef ASSIG_THREE
+#define ASSIG_THREE
+
+// Change the symbol below to see/hide output during debugging
+#define TRACING true
+// Change the symbol below to switch between depth- and breadth-first
+#define DEPTH_FIRST true
+// Change the symbol below to see/hide intermediate steps during the search
+#define SHOW_PROGRESS true
+
+void trace(char *s);
+
+#endif
